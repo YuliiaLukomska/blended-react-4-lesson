ef6a8438b619406f4c85007a5d45f1f066b2f909
NatalyMit <mitenkova91@icloud.com>
2024-04-06 16:21:22 +0200

    add components code

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,9 +1,29 @@
 import { Text } from 'components';
+import style from './Todo.module.css';
+import { RiDeleteBinLine } from 'react-icons/ri';
+import { useDispatch } from 'react-redux';
+import { deleteTodo } from 'reduxTodo/operations';
 
-export const Todo = () => {
+export const Todo = ({ text, id, counter }) => {
+  const dispatch = useDispatch();
+
   return (
-    <Text textAlign="center" marginBottom="20">
-      TODO #
-    </Text>
+    <div className={style.box}>
+      <Text textAlign="center" marginBottom="20">
+        TODO # {counter}
+      </Text>
+
+      <Text>{text}</Text>
+      <button
+        onClick={() => dispatch(deleteTodo(id))}
+        className={style.deleteButton}
+        type="button"
+      >
+        <RiDeleteBinLine size={24} />
+      </button>
+      {/* <button className={style.editButton} type="button">
+      <RiEdit2Line size={24} />
+    </button> */}
+    </div>
   );
 };
---
+++ src/reduxTodo/selectors.js
@@ -0,0 +1,3 @@
+export const selectTodos = state => state.todos.items;
+export const selectLoading = state => state.todos.loading;
+export const selectError = state => state.todos.error;
